869edf7e3ed90dc3e84a252d22b64965b530abf6
Joydeep Bhattacharjee <2jbhattacharjee@gmail.com>
2021-07-25 18:02:55 +0530

    swap.c added

--- array2.c
+++ array2.c
@@ -1,12 +1,21 @@
 #include<stdio.h>
+void change(int x, int y);
 int main()
 {
-
-	int a[5];
-	//a[0],a[1]...a[4]
-	printf("%u\n", &a[0]); //222
-	printf("%u\n", &a[1]); //226
-	printf("%u\n", &a[2]); //230
-	return 0;
-
+	int a[7] = {5, 1, 4, 9, 6, 3, 7};
+	//bubble sort
+	//it1
+	//1 4 5 6 3 7 9
+	//it2
+	//1 4 5 3 6 7 9
+	//it3
+	//1 4 3 5 6 7 9
+	//it4
+	//1 3 4 5 6 7 9
+	//it5
+	//1 3 6 7 9 
+	
 }
+
+
+//10
